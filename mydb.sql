8eb6d45f25c06b7b2b27a8e61a9262be078eff06
José Antonio Parejo Maestre <japarejo@gmail.com>
2018-11-07 18:26:26 +0100

    Configuración para derby

--- mydb.sql
+++ mydb.sql
@@ -1,65 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Oct 27, 2014 at 12:20 AM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+CREATE TABLE  credentials (
+  username varchar(20) NOT NULL DEFAULT '',
+  passwd varchar(32) DEFAULT NULL,
+  PRIMARY KEY (username)
+);
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `mydb`
---
-CREATE DATABASE `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `mydb`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `credentials`
---
-
-CREATE TABLE IF NOT EXISTS `credentials` (
-  `username` varchar(20) NOT NULL DEFAULT '',
-  `passwd` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `credentials`
---
-
-INSERT INTO `credentials` (`username`, `passwd`) VALUES
+INSERT INTO credentials (username, passwd) VALUES
 ('admin', 'admin'),
 ('test', 'test'),
 ('user', 'user');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `groups`
---
+CREATE TABLE groups (
+  id int NOT NULL,
+  username varchar(20) DEFAULT NULL,
+  groupname varchar(20) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE IF NOT EXISTS `groups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) DEFAULT NULL,
-  `groupname` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` (`id`, `username`, `groupname`) VALUES
+INSERT INTO groups (id, username, groupname) VALUES
 (4, 'test', 'registereduser'),
 (5, 'test', 'invitedguest'),
 (6, 'admin', 'invitedguest');
